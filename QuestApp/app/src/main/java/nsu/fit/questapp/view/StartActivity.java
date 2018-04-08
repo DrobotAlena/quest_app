ea6dff677f451d44a2f96bda4e3f33acea52a08b
DrobotAlena <VladAlena12345>
2018-04-08 13:31:37 +0300

    update start activity rename MainActivity to StartActivity add drawables, style, strings for start buttons and logo

--- QuestApp/app/src/main/java/nsu/fit/questapp/view/StartActivity.java
+++ QuestApp/app/src/main/java/nsu/fit/questapp/view/StartActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import nsu.fit.questapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
     }
 }
