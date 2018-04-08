e0c72fb43c14294d5725e623ccbc2b789fad5182
DrobotAlena <VladAlena12345>
2018-04-08 18:18:45 +0300

    add BottomSheetDialog

--- QuestApp/app/src/main/java/nsu/fit/questapp/view/QuestActivity.java
+++ QuestApp/app/src/main/java/nsu/fit/questapp/view/QuestActivity.java
@@ -2,8 +2,11 @@ package nsu.fit.questapp.view;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import nsu.fit.questapp.R;
 import nsu.fit.questapp.view.fragment.QuestFragment;
@@ -12,6 +15,9 @@ import nsu.fit.questapp.view.fragment.QuestFragment;
  * Created by Alena Drobot
  */
 public class QuestActivity extends AppCompatActivity {
+
+    private BottomSheetDialog sandwichMenu;
+    private Button sandwichButton;
 
     public static void start(Context context) {
         context.startActivity(new Intent(context, QuestActivity.class));
@@ -22,10 +28,29 @@ public class QuestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest);
 
+        initSandwichMenu();
+        initViews();
+
         // TODO: replace to method
         QuestFragment questFragment = new QuestFragment();
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.quest_fragment, questFragment)
                 .commit();
+    }
+
+    private void initViews() {
+        sandwichButton = findViewById(R.id.quest_sandwich);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        sandwichButton.setOnClickListener(v -> sandwichMenu.show());
+    }
+
+    private void initSandwichMenu() {
+        View view = getLayoutInflater().inflate(R.layout.bottom_sheet_menu, null);
+        sandwichMenu = new BottomSheetDialog(this);
+        sandwichMenu.setContentView(view);
     }
 }
--- QuestApp/app/src/main/java/nsu/fit/questapp/view/fragment/QuestFragment.java
+++ QuestApp/app/src/main/java/nsu/fit/questapp/view/fragment/QuestFragment.java
@@ -1,6 +1,7 @@
 package nsu.fit.questapp.view.fragment;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
